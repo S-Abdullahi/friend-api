674799fc47e5b4e18c2bd58b2c748f063515686f
Abdullahi Salawu <salabdullah33@gmail.coom>
2022-07-08 09:04:35 +0100

    friend api practice

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+PORT = 8000
+
+friend = {
+    'qudus': {
+        'name': 'Agbaje Abdulqudus',
+        'occupation': 'enterpreneur',
+        'interest' : 'cycling',
+        'staus': 'single'
+    },
+    'dabiri': {
+        'name': 'Dabiri Abdulhameed',
+        'occupation': 'Engineer',
+        'interest' : 'Sport',
+        'staus': 'single'
+    },
+    'olalekan': {
+        'name': 'Ogunsanya Abdulqudus',
+        'occupation': 'enterpreneur',
+        'interest' : 'software',
+        'staus': 'married'
+    }
+}
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:name',(req,res) => {
+    friendName = req.params.name.toLowerCase()
+    if (friend[friendName]) {
+        res.json(friend[friendName])
+    }})
+   
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`You are running on port ${PORT}. Go and catch it!`)
+})
